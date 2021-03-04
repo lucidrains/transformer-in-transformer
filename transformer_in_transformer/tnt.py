fix pixel calculation bugs

transformer_in_transformer/tnt.py
@@ -97,10 +97,10 @@ class TNT(nn.Module):
         self.patch_tokens = nn.Parameter(torch.randn(num_patch_tokens + 1, patch_dim))
 
         self.to_pixel_tokens = nn.Sequential(
-            Rearrange('b c (p1 h) (p2 w) -> (b h w) c p1 p2', p1 = patch_size, p2 = patch_size),
-            nn.Unfold(pixel_width, stride = pixel_width),
+            Rearrange('b c (h p1) (w p2) -> (b h w) c p1 p2', p1 = patch_size, p2 = patch_size),
+            nn.Unfold(pixel_size, stride = pixel_size),
             Rearrange('... c n -> ... n c'),
-            nn.Linear(3 * pixel_width ** 2, pixel_dim)
+            nn.Linear(3 * pixel_size ** 2, pixel_dim)
         )
 
         self.patch_pos_emb = nn.Parameter(torch.randn(num_patch_tokens + 1, patch_dim))