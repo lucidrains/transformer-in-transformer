a63e4ade2be497ef09da3abafb967e1a1deb0470
Phil Wang <lucidrains@gmail.com>
2021-03-23 08:20:13 -0700

    back to layernorm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'transformer-in-transformer',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'Transformer in Transformer - Pytorch',
   author = 'Phil Wang',
--- transformer_in_transformer/tnt.py
+++ transformer_in_transformer/tnt.py
@@ -21,21 +21,10 @@ def unfold_output_size(image_size, kernel_size, stride, padding):
 
 # classes
 
-class RMSNorm(nn.Module):
-    def __init__(self, dim, eps = 1e-5):
-        super().__init__()
-        self.scale = dim ** -0.5
-        self.eps = eps
-        self.g = nn.Parameter(torch.ones(dim))
-
-    def forward(self, x):
-        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
-        return x / norm.clamp(min = self.eps) * self.g
-
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
-        self.norm = RMSNorm(dim)
+        self.norm = nn.LayerNorm(dim)
         self.fn = fn
 
     def forward(self, x, **kwargs):
@@ -137,7 +126,7 @@ class TNT(nn.Module):
         for _ in range(depth):
 
             pixel_to_patch = nn.Sequential(
-                RMSNorm(pixel_dim),
+                nn.LayerNorm(pixel_dim),
                 Rearrange('... n d -> ... (n d)'),
                 nn.Linear(pixel_dim * num_pixels, patch_dim),
             )
@@ -153,7 +142,7 @@ class TNT(nn.Module):
         self.layers = layers
 
         self.mlp_head = nn.Sequential(
-            RMSNorm(patch_dim),
+            nn.LayerNorm(patch_dim),
             nn.Linear(patch_dim, num_classes)
         )
 
